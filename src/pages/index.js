69c9b60452377d01e8fe1f873bf7d388c608202a
jacob kleiman <jacobakleiman@gmail.com>
2021-01-20 22:18:44 -0800

    adds Layout to home, installs emotion/react

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 // markup
 export default () => {
   return (
-    <>
+    <Layout>
       <h1>Home</h1>
       <Link to='/about'>About</Link>
       <Link to='/merch'>Merch</Link>
       <Link to='/shows'>Shows</Link>
       <Link to='/contact'>Contact</Link>
-    </>
+    </Layout>
   );
 };
